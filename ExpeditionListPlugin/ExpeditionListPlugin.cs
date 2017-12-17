e305332b70c36d83ab1710b458687461dad45fe9
ruhiel_murrue <ruhiel1983@gmail.com>
2017-12-17 21:05:14 +0900

    v1.0.6

--- ExpeditionListPlugin/ExpeditionListPlugin.cs
+++ ExpeditionListPlugin/ExpeditionListPlugin.cs
@@ -9,7 +9,7 @@ namespace ExpeditionListPlugin
     [Export(typeof(IRequestNotify))]
     [ExportMetadata("Title", "遠征一覧プラグイン")]
     [ExportMetadata("Description", "遠征一覧と各艦隊が成功条件を満たすか表示します。")]
-    [ExportMetadata("Version", "1.0.5")]
+    [ExportMetadata("Version", "1.0.6")]
     [ExportMetadata("Author", "@ruhiel_murrue")]
     [ExportMetadata("Guid", "B8BDDEA7-1AEC-420B-8F6D-8F4EE906DC1B")]
     public class ExpeditionListPlugin : IPlugin, ITool, IRequestNotify
