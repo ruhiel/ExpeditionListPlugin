8b03f44a0245188eda6948e77a8327032141cc0d
ruhiel_murrue <ruhiel1983@gmail.com>
2017-12-23 22:15:06 +0900

    テストコード整理

--- ExpeditionListPluginTests/ExpeditionInfoTests.cs
+++ ExpeditionListPluginTests/ExpeditionInfoTests.cs
@@ -8,44 +8,44 @@ using System.Threading.Tasks;
 
 namespace ExpeditionListPlugin.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class ExpeditionInfoTests
     {
-        [TestMethod()]
+        [TestMethod]
         public void RequireShipTypeTextTest()
         {
-            var s = ExpeditionInfo._ExpeditionTable[3].RequireShipTypeText;
+            var s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "対潜警戒任務").RequireShipTypeText;
             Console.WriteLine(s);
-            s.Is("軽1 駆2 or 駆1 海防3 or 軽1 海防2 or 練1 海防2 or 護母1 海防2 or 護母1 駆2 ");
+            Assert.AreEqual("軽1 駆2 or 駆1 海防3 or 軽1 海防2 or 練1 海防2 or 護母1 海防2 or 護母1 駆2 ", s);
 
-            s = ExpeditionInfo._ExpeditionTable[8].RequireShipTypeText;
+            s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "兵站強化任務").RequireShipTypeText;
             Console.WriteLine(s);
-            s.Is("駆,海防合計3");
+            Assert.AreEqual("駆,海防合計3", s);
 
             s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "長時間対潜警戒").RequireShipTypeText;
             Console.WriteLine(s);
-            s.Is("軽1 駆,海防合計3");
+            Assert.AreEqual("軽1 駆,海防合計3", s);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void RequireSumParamText()
         {
             string s;
             s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "兵站強化任務").RequireSumParamText;
             Console.WriteLine(s);
-            s.Is("");
+            Assert.AreEqual("", s);
 
             s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "海峡警備行動").RequireSumParamText;
             Console.WriteLine(s);
-            s.Is("対空70/対潜180");
+            Assert.AreEqual("対空70/対潜180", s);
 
             s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "長時間対潜警戒").RequireSumParamText;
             Console.WriteLine(s);
-            s.Is("対潜280");
+            Assert.AreEqual("対潜280", s);
 
             s = ExpeditionInfo._ExpeditionTable.First(t => t.EName == "南西方面航空偵察作戦").RequireSumParamText;
             Console.WriteLine(s);
-            s.Is("対空200/対潜200/索敵140");
+            Assert.AreEqual("対空200/対潜200/索敵140", s);
         }
     }
 }
